8e0bb6bf09325051d655784cba1683f47c310b71
mattg95 <mattgoodman95@gmail.com@>
2020-12-12 20:10:58 +0000

    setup app.js
    -add reset.css
    
    -added sass
    
    added bootstrap

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.scss";
+import { Container, Row, Col } from "react-bootstrap";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Container>
+        <Row>
+          <Col>
+            <h1 className="text-center">0.7% Campaign</h1>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <p className="text-center">
+              Info about our campaign. Lorem ipsum dolor sit amet, consectetur
+              adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+              dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+              exercitation ullamco laboris nisi ut aliquip ex ea commodo
+              consequat.
+            </p>
+          </Col>
+        </Row>
+        <Row>
+          <Col>{/* Form elements go here */}</Col>
+        </Row>
+      </Container>
     </div>
+    // Cookie banner here
   );
 }
 
