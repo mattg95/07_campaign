1f7717020a895c8a4f1b32a92e35645c41fdaf93
Caleb Parikh <parikhcaleb@gmail.com>
2020-12-13 22:48:33 +0000

    refactor into seperate mail generator method

--- react-app/src/PostCode.js
+++ react-app/src/PostCode.js
@@ -17,6 +17,22 @@ function postcodeToConstituencyAPIReq(postcode){
   return TWFY_API+'getMp?key='+KEY+'&postcode='+postcode+'&output=js'
 }
 
+function DisplayEmail(props){
+  const mp = props.mp;
+  console.log(mp)
+  if(mp!==' '){
+    return(
+        <div>
+          <a href={"mailto:" + mp.replace(" ",".")+".mp@parliament.uk?Subject=stop%20taking%20away%20poor%20people%27s%20money&Body=please%20please%20please%20be%20a%20reasonable%20person"}>
+            {mp.replace(" ",".")+".mp@parliament.uk"}
+          </a>
+        </div>
+      )
+  }
+  return(
+      <div></div>
+    )
+}
 
 
 class PostcodeForm extends React.Component {
@@ -36,12 +52,14 @@ class PostcodeForm extends React.Component {
     console.log(this.state.value)
     event.preventDefault();
     console.log(postcodeToConstituencyAPIReq(this.state.value))
-    axios.get(postcodeToConstituencyAPIReq(this.state.value)).then(response => {
+    axios.get(postcodeToConstituencyAPIReq(this.state.value))
+    .then(response => {
       this.setState({constituency: response.data.constituency});
       this.setState({mp: response.data.full_name});
       this.setState({party: response.data.party});
       console.log(response);
-    }).then(() => console.log(this.state.constituency))
+    })
+    .then(() => console.log(this.state.constituency))
       .catch(error => console.error('On get constituency error', error))
   }
 
@@ -56,11 +74,8 @@ class PostcodeForm extends React.Component {
       <div>{this.state.constituency}</div>
       <div>{this.state.mp}</div>
       <div>{this.state.party}</div>
-      <div>
-        <a href={"mailto:" + this.state.mp.replace(" ",".")+".mp@parliament.uk?Subject=stop%20taking%20away%20poor%20people%27s%20money&Body=please%20please%20please%20be%20a%20reasonable%20person"}>
-          {this.state.mp.replace(" ",".")+".mp@parliament.uk"}
-        </a>
-      </div>
+
+      <DisplayEmail mp={this.state.mp}/>
 
     </form>
   )
