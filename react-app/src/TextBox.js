b633a59701a4b01a1a79499d026456911ebba06f
mattg95 <mattgoodman95@gmail.com@>
2020-12-16 09:13:15 +0000

    add survey questions

--- react-app/src/TextBox.js
+++ react-app/src/TextBox.js
@@ -1,39 +1,101 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import EdiText from "react-editext";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import Select from "react-select";
+
 import PostCode from "./PostCode.js";
 
-class TextBox extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      value:
-        "Email to MP. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-    };
-  }
-  onSave = (val) => {
-    console.log("Edited Value -> ", val);
-    this.setState({ value: val });
-  };
+const TextBox = () => {
+  const [email, setEmail] = useState("");
 
-  render() {
-    return (
-      <div>
-        <EdiText
-          viewContainerClassName="my-custom-view-wrapper"
-          type="textarea"
-          inputProps={{
-            rows: 5,
-          }}
-          saveButtonContent="Apply"
-          cancelButtonContent={<strong>Cancel</strong>}
-          editButtonContent="Edit Your Email"
-          value={this.state.value}
-          onSave={this.onSave}
-        />
-        <PostCode body={this.state.value} subject={"subject"} />
-      </div>
-    );
-  }
-}
+  const q1options = [
+    { value: "International development is good. ", label: "good" },
+    { value: "International development is bad. ", label: "bad" },
+    {
+      value: "International development is flippin rad. ",
+      label: "flippin rad",
+    },
+  ];
+  const q2options = [
+    { value: "I am a member of the Conservative Party. ", label: "yes" },
+    { value: "I am not a member of the Conservative Party. ", label: "no" },
+    {
+      value: "I am maybe a member of the Conservative Party. ",
+      label: "maybe",
+    },
+  ];
+  const q3options = [
+    {
+      value:
+        "I am angry about the conservatives breaking their manifesto promises. ",
+      label: "anger",
+    },
+    {
+      value:
+        "I am confused about the conservatives breaking their manifesto promises. ",
+      label: "confusion",
+    },
+    {
+      value:
+        "I am not angry, just dissapointed about the conservatives breaking their manifesto promises. ",
+      label: "not angry, just dissapointed",
+    },
+  ];
+
+  return (
+    <div>
+      <Formik>
+        <Form>
+          <label htmlFor="q1">
+            What are your thoughs on International Development?
+          </label>
+          <Select
+            options={q1options}
+            name="q1"
+            onChange={({ value }) => {
+              setEmail(email + value);
+            }}
+          />
+          <label htmlFor="q2">
+            Are you a member of the Conservative party?
+          </label>
+          <Select
+            options={q2options}
+            name="q2"
+            onChange={({ value }) => {
+              setEmail(email + value);
+            }}
+          />
+          <label htmlFor="q3">
+            How do you feel about the Consevatives breaking their manifesto
+            promises?
+          </label>
+          <Select
+            options={q3options}
+            name="q3"
+            onChange={({ value }) => {
+              setEmail(email + value);
+            }}
+          />
+        </Form>
+      </Formik>
+      <EdiText
+        viewContainerClassName="my-custom-view-wrapper"
+        type="textarea"
+        inputProps={{
+          rows: 5,
+        }}
+        saveButtonContent="Apply"
+        cancelButtonContent={<strong>Cancel</strong>}
+        editButtonContent="Edit Your Email"
+        value={email}
+        onSave={(val) => {
+          setEmail(val);
+        }}
+      />
+      <PostCode body={email} subject={"subject"} />
+    </div>
+  );
+};
 
 export default TextBox;
