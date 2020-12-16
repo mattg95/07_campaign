3f34d26e5b732aeea3302d920283e8edaa98cd3c
mattg95 <mattgoodman95@gmail.com@>
2020-12-16 10:47:31 +0000

    change object method, add comments

--- react-app/src/TextBox.js
+++ react-app/src/TextBox.js
@@ -14,39 +14,38 @@ const TextBox = () => {
   });
 
   const q1options = [
-    { value: "International development is good. ", label: "good" },
-    { value: "International development is bad. ", label: "bad" },
+    { value: "International development is good.", label: "good" },
+    { value: "International development is bad.", label: "bad" },
     {
-      value: "International development is flippin rad. ",
+      value: "International development is flippin rad.",
       label: "flippin rad",
     },
   ];
   const q2options = [
-    { value: "I am a member of the Conservative Party. ", label: "yes" },
+    { value: "I am a member of the Conservative Party.", label: "yes" },
     { value: "I am not a member of the Conservative Party. ", label: "no" },
     {
-      value: "I am maybe a member of the Conservative Party. ",
+      value: "I am maybe a member of the Conservative Party.",
       label: "maybe",
     },
   ];
   const q3options = [
     {
       value:
-        "I am angry about the conservatives breaking their manifesto promises. ",
+        "I am angry about the conservatives breaking their manifesto promises.",
       label: "anger",
     },
     {
       value:
-        "I am confused about the conservatives breaking their manifesto promises. ",
+        "I am confused about the conservatives breaking their manifesto promises.",
       label: "confusion",
     },
     {
       value:
-        "I am not angry, just dissapointed about the conservatives breaking their manifesto promises. ",
+        "I am not angry, just dissapointed about the conservatives breaking their manifesto promises.",
       label: "not angry, just dissapointed",
     },
   ];
-  console.log(state);
 
   return (
     <div>
@@ -59,7 +58,7 @@ const TextBox = () => {
             options={q1options}
             name="q1"
             onChange={({ value }) => {
-              setState({ ...state, q1Response: value });
+              setState({ ...state, q1Response: value }); //adds data to state on change
             }}
           />
           <label htmlFor="q2">
@@ -69,7 +68,7 @@ const TextBox = () => {
             options={q2options}
             name="q2"
             onChange={({ value }) => {
-              setState({ ...state, q2Response: value });
+              setState({ ...state, q2Response: value }); //adds data to state on change
             }}
           />
           <label htmlFor="q3">
@@ -80,7 +79,7 @@ const TextBox = () => {
             options={q3options}
             name="q3"
             onChange={({ value }) => {
-              setState({ ...state, q3Response: value });
+              setState({ ...state, q3Response: value }); //adds data to state on change
             }}
           />
         </Form>
@@ -94,9 +93,9 @@ const TextBox = () => {
         saveButtonContent="Apply"
         cancelButtonContent={<strong>Cancel</strong>}
         editButtonContent="Edit Your Email"
-        value={Object.values(state).toString()}
+        value={Object.values(state).join(" ")} //converts state object into a string to display
         onSave={(val) => {
-          setState({ editedResponse: val });
+          setState({ editedResponse: val }); //if the user edits the text box, a new property called editedResponse is set in state
         }}
       />
       <PostCode body={state} subject={"subject"} />
