8984cfb2a8d97aedd187b80d7595b3cdbdf554e0
mattg95 <mattgoodman95@gmail.com@>
2020-12-23 10:29:09 +0000

    tidy up UX and code

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import "./App.scss";
 import { Container, Row, Col } from "react-bootstrap";
 import TextBox from "./TextBox.js";
 
-require('dotenv').config({ path: '../.env' })
+require("dotenv").config({ path: "../.env" });
 
 function App() {
   return (
@@ -20,7 +20,13 @@ function App() {
               adipiscing elit, sed do eiusmod tempor incididunt ut labore et
               dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
               exercitation ullamco laboris nisi ut aliquip ex ea commodo
-              consequat.
+              consequat.Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+              sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat
+              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+              sunt in culpa qui officia deserunt mollit anim id est laborum.
             </p>
           </Col>
         </Row>
