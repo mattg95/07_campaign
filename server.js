d5eb0eace70872da7f60f783baf9765545221719
mattg95 <mattgoodman95@gmail.com@>
2021-01-07 14:41:54 +0000

    allow cors

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+var cors = require('cors');
+
 
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+
 
 app.get('/api/hello', (req, res) => {
   res.send({ express: 'Hello From Express' });
