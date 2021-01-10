f8279d996e9487d2d1e3b972ba27875e64cad09b
mattg95 <mattgoodman95@gmail.com@>
2021-01-10 10:49:17 +0000

    add build scripts

--- README.md
+++ README.md
@@ -8,4 +8,4 @@
 -Run `npm i` (installs node modules)
 -Run `npm run install-react` (installs react's node modules)
 -Run `npm run server` to start express server (available at localhost:5000)
--Run `npm run react` to start React app (avaliable at localhost:3000)
+-Run `npm run react-start` to start React app (avaliable at localhost:3000)
--- server.js
+++ server.js
@@ -1,14 +1,20 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-var cors = require("cors");
+const cors = require("cors");
+const app = express();
+const path = require("path");
 const { getMpByPostcode } = require("./api-functions");
 
-const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
+app.use(express.static(path.join("./react-app", "build")));
+
+app.get("/*", (req, res) => {
+  res.sendFile(path.join("./react-app", "build", "index.html"));
+});
 
 app.get("/api/postcode/:postcodeInput", (req, res) => {
   getMpByPostcode(req.params.postcodeInput).then((response) =>
