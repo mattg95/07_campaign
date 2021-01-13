bef8fb938c480f3fafb0f74e1e36dcb991d17a01
mattg95 <mattgoodman95@gmail.com@>
2021-01-13 11:23:54 +0000

    validate webhook and pass to text box

--- react-app/src/TypeForm.js
+++ react-app/src/TypeForm.js
@@ -1,8 +1,10 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import * as typeformEmbed from "@typeform/embed";
+import TextBox from "./TextBox";
 
 const TypeForm = () => {
   const myRef = useRef(null);
+  const [state, setState] = useState("");
 
   useEffect(() => {
     typeformEmbed.makeWidget(
@@ -12,11 +14,17 @@ const TypeForm = () => {
         hideFooter: true,
         hideHeaders: true,
         opacity: 0,
+        onSubmit: ({ response_id }) => setState(response_id),
       }
     );
   }, [myRef]);
 
-  return <div ref={myRef} className="typeform-widget" />;
+  return (
+    <div>
+      <div ref={myRef} className="typeform-widget" />
+      <TextBox responseId={state} />
+    </div>
+  );
 };
 
 export default TypeForm;
