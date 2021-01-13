32c808a58ed5a55c686f013601a864a0f90c7161
mattg95 <mattgoodman95@gmail.com@>
2021-01-13 11:23:54 +0000

    validate webhook and pass to text box

--- react-app/src/TextBox.js
+++ react-app/src/TextBox.js
@@ -6,18 +6,19 @@ const emailStrings = require("./emailStrings.json");
 
 const socket = socketIOClient();
 
-socket.on("typeform-incoming", function ({ data }) {
-  console.log(data);
-});
-const TextBox = () => {
+const TextBox = ({ responseId }) => {
+  //the responseId comes from the embeded typeform
   const [state, setState] = useState({
-    response: "",
-    post: "",
-    responseToPost: "",
+    formToken: "", //the form token comes from the webhook response
   });
-  useEffect(() => {}, []);
 
-  console.log(state);
+  console.log(state.formToken, responseId);
+
+  useEffect(() => {
+    socket.on("typeform-incoming", function ({ data }) {
+      setState({ formToken: data.form_response.token });
+    });
+  }, []);
 
   const getRandomRes = (inputArr) => {
     const randomNum = Math.floor(Math.random() * inputArr.length);
@@ -35,10 +36,10 @@ const TextBox = () => {
       saveButtonContent="Apply"
       cancelButtonContent={<strong>Cancel</strong>}
       editButtonContent="Edit Your Email"
-      // value={Object.values(state).join(" ")} //converts state object into a string to display
-      onSave={(val) => {
-        setState({ ...state, editedRes: val }); //if the user edits the text box, a new property called editedResponse is set in state
-      }}
+      value={responseId === state.formToken ? "data" : ""} // validates the webhook response token against the response id from the embedded tyeform widget
+      // onSave={(val) => {
+      //   setState({ ...state, editedRes: val }); //if the user edits the text box, a new property called editedResponse is set in state
+      // }}
     />
   );
 };
