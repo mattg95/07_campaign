8754c555d0131b5ab08843a177454327a9988107
mattg95 <mattgoodman95@gmail.com>
2021-01-14 12:09:52 +0000

    move to key based fetching

--- formResponseHandler.js
+++ formResponseHandler.js
@@ -1,56 +1,48 @@
 const emailStrings = require("./emailStrings.json");
 
 const generateEmail = (formResponseData) => {
-  // gil6UCe4dG9T : "I believe that it’s good for the UK government to provide aid to people in the developing world",
-  // wKGNjgRDml1H : " Which of the following best reflect your motivations for supporting international aid/development?",
-  // nfkfl48YcQLu : I have links with a country that is receiving or might receive aid",
-  // MRPxTl6j1QAw : "Which country do you have links to?",
-  // Z4awe4sDljLR : Which of the following best describes the links you have to this country?",
-  // IdqRPd6SUMVh : "Which of the following religious backgrounds do you most identify with?",
-  // EejpFBEzP9wK : Which of the following best describes your relationship with the Conservative Party?\n",
-  // vdZgYVyiLE13 : Would you be willing to have a call/meeting with your MP?",
-  // ghzBmQTQ2npF : Could you share your email address with us?",
-  // uLPPjjg5B0Bn : Please enter your home address. ",
-  // daZZA6TwyMP5 : "What is your name?",
-
-  //this supports the order, do not remove unused variables
-  const questionKeys = [
-    "supportAid",
-    "countyLinks",
-    "whichCountry",
-    "whatLinks",
-    "conservative",
-    "religion",
-    "meetMp",
-    "emailAddress",
-    "homeAddress",
-    "name",
-  ];
+  const questionKeys = {
+    gil6UCe4dG9T: "supportAid",
+    wKGNjgRDml1H: "motivations",
+    nfkfl48YcQLu: "countryLinks",
+    MRPxTl6j1QAw: "whichCountry",
+    Z4awe4sDljLR: "typeofLinks",
+    EejpFBEzP9wK: "conservative",
+    IdqRPd6SUMVh: "religion",
+    vdZgYVyiLE13: "meetMp",
+    ghzBmQTQ2npF: "emailAddress",
+    uLPPjjg5B0Bn: "homeAddress",
+    daZZA6TwyMP5: "name",
+  };
   const answers = formResponseData.answers;
   const fields = formResponseData.definition.fields;
+
+  const emailArr = [];
 
   const getRandomResponse = (inputArr) => {
     return inputArr[Math.floor(Math.random() * inputArr.length)];
   };
 
-  const emailArr = [];
-
-  const getAnswerSynoymns = (questionIndex) => {
+  const getAnswerSynoymns = (idProp) => {
+    const thisField = fields.find(({ id }) => id === idProp);
+    const thisAnswers = answers.find(({ field: { id } }) => id === idProp);
     let choiceIndex = 0;
-    fields[questionIndex].choices.forEach((choice, i) => {
-      if (answers[questionIndex].choice.label === choice.label) {
+    thisField.choices.forEach((choice, i) => {
+      if (thisAnswers.choice.label === choice.label) {
         choiceIndex = i;
       }
     });
-    return emailStrings.survey[questionKeys[questionIndex]][choiceIndex];
+    console.log(questionKeys[idProp]);
+    return emailStrings.survey[questionKeys[idProp]] ? [choiceIndex] : [];
   };
-  console.log(getAnswerSynoymns(4));
+
+  console.log(formResponseData);
   answers.forEach((answer) => {
     if (answer.field.id === "EejpFBEzP9wK") {
-      emailArr.push(getRandomResponse(getAnswerSynoymns(4)));
+      emailArr.push(getRandomResponse(getAnswerSynoymns("EejpFBEzP9wK")));
     }
     if (answer.field.id === "IdqRPd6SUMVh") {
-      //religionHandler
+      // emailArr.push(getRandomResponse(getAnswerSynoymns(5)));
     }
     if (answer.field.id === "MRPxTl6j1QAw") {
       //countryNameHandler
