94f031794681ade55d740eafcf0de3ac0ce9103a
mattg95 <mattgoodman95@gmail.com@>
2021-01-15 16:41:45 +0000

    exclude non-religious responses

--- tests/spec.js
+++ tests/spec.js
@@ -2,17 +2,25 @@ process.env.NODE_ENV = "test";
 const { expect } = require("chai");
 const { generateEmail } = require("../formResponseHandler");
 
+const example0 = require("./exampleResponses/example0.json");
 const example1 = require("./exampleResponses/example1.json");
 const example2 = require("./exampleResponses/example2.json");
 const example3 = require("./exampleResponses/example3.json");
 const example4 = require("./exampleResponses/example4.json");
 
+const example0Res = generateEmail(example0.form_response);
 const example1Res = generateEmail(example1.form_response);
 const example2Res = generateEmail(example2.form_response);
 const example3Res = generateEmail(example3.form_response);
 const example4Res = generateEmail(example4.form_response);
 
-const allExampleRes = [example1Res, example2Res, example3Res, example4Res];
+const allExampleRes = [
+  example0Res,
+  example1Res,
+  example2Res,
+  example3Res,
+  example4Res,
+];
 
 describe("/api/postcode", () => {
   //tests for api route
@@ -47,6 +55,21 @@ describe("generateEmail", () => {
   it("should not include '[RELIGION]' template variable", () => {
     allExampleRes.forEach((res) => {
       expect(res.body.search(/\[RELIGION\]/)).to.equal(-1);
+    });
+  });
+  it("should not include a response for 'no religion' choice", () => {
+    allExampleRes.forEach((res) => {
+      console.log(res);
+      expect(res.body.search(/Not religious/)).to.equal(-1);
+      expect(res.body.search(/agnostic/)).to.equal(-1);
+      expect(res.body.search(/athiest/)).to.equal(-1);
+    });
+  });
+  it("should not include a response for 'other religion' choice", () => {
+    allExampleRes.forEach((res) => {
+      expect(res.body.search(/Not religious/)).to.equal(-1);
+      expect(res.body.search(/agnostic/)).to.equal(-1);
+      expect(res.body.search(/athiest/)).to.equal(-1);
     });
   });
 });
