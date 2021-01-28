b2f9712bac00081d7644f829a3869d0f000a61f4
mattg95 <mattgoodman95@gmail.com>
2021-01-28 16:08:30 +0000

    style evertyhing

--- react-app/src/IntroContent.jsx
+++ react-app/src/IntroContent.jsx
@@ -3,7 +3,7 @@ import React from "react";
 // import ArrowDown from "./assets/arrow-down.svg";
 
 const IntroContent = () => (
-  <div>
+  <div className="intro-content">
     <h1 className="title">0.7% Campaign</h1>
     <p className="intro-para">
       Info about our campaign. Lorem ipsum dolor sit amet, consectetur
