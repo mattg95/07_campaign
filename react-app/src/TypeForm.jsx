8c75f62b6cee06a4112f25f338631340efe473e8
mattg95 <mattgoodman95@gmail.com>
2021-02-04 21:11:01 +0000

    set smooth scroll on typeform end

--- react-app/src/TypeForm.jsx
+++ react-app/src/TypeForm.jsx
@@ -1,22 +1,29 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import * as typeformEmbed from "@typeform/embed";
 
 const TypeForm = ({ passDataUpstream }) => {
   const myRef = useRef(null);
   const mpForm = document.getElementById("displayMP");
+  const [isOpen, setIsOpen] = useState(true);
 
   useEffect(() => {
     typeformEmbed.makeWidget(
       myRef.current,
       `https://z8ivgb8lhnl.typeform.com/to/YbkRDwtc`,
       {
-        hideFooter: true,
-        hideHeaders: true,
+        hideFooter: false,
+        hideHeaders: false,
         opacity: 0,
         onSubmit: ({ response_id }) => {
           passDataUpstream({ responseId: response_id });
           setTimeout(() => {
-            mpForm && mpForm.scrollIntoView();
+            setIsOpen(false);
+          }, 2000);
+          setTimeout(() => {
+            myRef.current.scrollIntoView({
+              behavior: "smooth",
+              block: "start",
+            });
           }, 3000);
         },
       }
@@ -26,7 +33,11 @@ const TypeForm = ({ passDataUpstream }) => {
   return (
     <div>
       {/* potentially use state to close typform widget after completion */}
-      <div ref={myRef} className={`typeform-widget `} id="typeform" />
+      <div
+        ref={myRef}
+        className={`typeform-widget ${isOpen ? "" : "closed"}`}
+        id="typeform"
+      />
     </div>
   );
 };
