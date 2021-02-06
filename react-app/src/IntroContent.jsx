31af23d1d4a4524a7b000df00009818ac93593e2
mattg95 <mattgoodman95@gmail.com@>
2021-02-06 16:26:17 +0000

    add survey-start cta

--- react-app/src/IntroContent.jsx
+++ react-app/src/IntroContent.jsx
@@ -10,12 +10,6 @@ const IntroContent = () => (
       adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
       magna aliqua.
     </p>
-    <div className="">
-      <h2 className="secondary-header">1. Fill out the form</h2>
-      <p className="explanation">
-        This will generate an email to send to your MP
-      </p>
-    </div>
   </div>
 );
 export default IntroContent;
