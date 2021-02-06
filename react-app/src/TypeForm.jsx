74e2e45aee4e3214b0697c710f63e0017f8973da
mattg95 <mattgoodman95@gmail.com@>
2021-02-06 16:49:45 +0000

    stop popup constantly opening -rename ref

--- react-app/src/TypeForm.jsx
+++ react-app/src/TypeForm.jsx
@@ -2,15 +2,26 @@ import React, { useEffect, useRef, useState } from "react";
 import * as typeformEmbed from "@typeform/embed";
 
 const TypeForm = ({ passDataUpstream, isMobile }) => {
-  const myRef = useRef(null);
+  const typeformComponent = useRef(null);
   const buttonRef = useRef(null);
-  let mpForm;
   const [typeformWidgetOpen, setTypeformWidgetOpen] = useState(true);
+
+  const mobileTypeform = typeformEmbed.makePopup(
+    `https://z8ivgb8lhnl.typeform.com/to/YbkRDwtc`,
+    {
+      mode: "popup",
+      openValue: 50,
+      autoClose: 3,
+      onSubmit: ({ response_id }) => {
+        passDataUpstream({ responseId: response_id });
+      },
+    }
+  );
 
   useEffect(() => {
     !isMobile &&
       typeformEmbed.makeWidget(
-        myRef.current,
+        typeformComponent.current,
         `https://z8ivgb8lhnl.typeform.com/to/YbkRDwtc`,
         {
           hideScrollbars: true,
@@ -23,28 +34,23 @@ const TypeForm = ({ passDataUpstream, isMobile }) => {
           },
         }
       );
-    const mobileTypeform = typeformEmbed.makePopup(
-      `https://z8ivgb8lhnl.typeform.com/to/YbkRDwtc`,
-      {
-        mode: "popup",
-        openValue: 50,
-        autoClose: 3,
-        onSubmit: ({ response_id }) => {
-          passDataUpstream({ responseId: response_id });
-        },
-      }
-    );
-    buttonRef.current.addEventListener("click", () => {
-      isMobile
-        ? mobileTypeform.open()
-        : myRef.current.scrollIntoView({ behavior: "smooth", block: "end" });
-    });
-  }, [myRef, mpForm, passDataUpstream, isMobile]);
+  }, [typeformComponent, passDataUpstream, isMobile]);
 
   return (
     <div>
       <div className="call-to-action text-center">
-        <button ref={buttonRef} className="btn btn-primary btn-lg">
+        <button
+          ref={buttonRef}
+          onClick={() => {
+            isMobile
+              ? mobileTypeform.open()
+              : typeformComponent.current.scrollIntoView({
+                  behavior: "smooth",
+                  block: "end",
+                });
+          }}
+          className="btn btn-primary btn-lg"
+        >
           1. Fill out the form
         </button>
         <p className="explanation">
@@ -52,7 +58,7 @@ const TypeForm = ({ passDataUpstream, isMobile }) => {
         </p>
       </div>
       <div
-        ref={myRef}
+        ref={typeformComponent}
         className={`typeform-widget ${typeformWidgetOpen ? "" : "closed"}`}
         id="typeform"
       />
