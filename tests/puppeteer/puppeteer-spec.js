ea71a309f5b3a38f96e300f43813d555f1caaeda
mattg95 <mattgoodman95@gmail.com@>
2021-02-09 16:48:36 +0000

    send mp Email address to front end
    -change data strucutre in FE to mirror new api result
    -add tests for mp email addresses
    -fix failing puppeteer tests
    -(auto format puppeteer file)

--- tests/puppeteer/puppeteer-spec.js
+++ tests/puppeteer/puppeteer-spec.js
@@ -1,6 +1,6 @@
-const { expect } = require('chai');
+const { expect } = require("chai");
 
-const LIST_ITEMS_SELECTOR = 'li';
+const LIST_ITEMS_SELECTOR = "li";
 
 const getTypeformFrame = (page) => {
   for (const frame of page.frames()) {
@@ -8,36 +8,39 @@ const getTypeformFrame = (page) => {
       return frame;
     }
   }
-}
+};
 
-describe('Puppeteer tests', function () {
+describe("Puppeteer tests", function () {
   // Increase timeout from default of 2s to allow time for page to load.
   this.timeout(5000);
   let page;
 
-  before (async function () {
+  before(async function () {
     page = await browser.newPage();
   });
 
   beforeEach(async function () {
-    await page.goto('http://localhost:3000');
-  })
+    await page.goto("http://localhost:3000");
+  });
 
-  after (async function () {
+  after(async function () {
     await page.close();
-  })
+  });
 
-  it('should have a heading that says 0.7% Campaign', async function () {
-    const HEADING_SELECTOR = 'h1';
+  it("should have a heading that says 0.7% Campaign", async function () {
+    const HEADING_SELECTOR = "h1";
     let heading;
 
     await page.waitForSelector(HEADING_SELECTOR);
-    heading = await page.$eval(HEADING_SELECTOR, heading => heading.innerText);
+    heading = await page.$eval(
+      HEADING_SELECTOR,
+      (heading) => heading.innerText
+    );
 
-    expect(heading).to.eql('0.7% Campaign');
+    expect(heading).to.eql("The 0.7% Campaign");
   });
 
-  it('should stop the survey if the user answers No to the first question', async function () {
+  it("should stop the survey if the user answers No to the first question", async function () {
     const THANK_YOU_SPAN_SELECTOR = '[data-qa="thank-you-screen-wrapper"] span';
     const typeformFrame = getTypeformFrame(page);
 
@@ -49,32 +52,36 @@ describe('Puppeteer tests', function () {
         }
       }
     });
-    
-    await typeformFrame.waitForSelector(THANK_YOU_SPAN_SELECTOR, {timeout: 3000});
-    text = await typeformFrame.$eval(THANK_YOU_SPAN_SELECTOR, span => {
-      return span.innerHTML
+
+    await typeformFrame.waitForSelector(THANK_YOU_SPAN_SELECTOR, {
+      timeout: 3000,
+    });
+    text = await typeformFrame.$eval(THANK_YOU_SPAN_SELECTOR, (span) => {
+      return span.innerHTML;
     });
     expect(text).to.contain("Thanks for taking this survey.");
   });
 
-  it('should continue the survey if the user answers Yes to the first question', async function () {
+  it("should continue the survey if the user answers Yes to the first question", async function () {
     const NEXT_QUESTION_SELECTOR = 'div[tabindex="0"]';
     const typeformFrame = getTypeformFrame(page);
 
     await typeformFrame.waitForSelector(LIST_ITEMS_SELECTOR, { timeout: 3000 });
 
     await typeformFrame.$$eval(LIST_ITEMS_SELECTOR, (listItems) => {
-        for (listItem of listItems) {
-          if (listItem.innerText.includes("Yes")) {
-            listItem.click();
-            return;
-          }
+      for (listItem of listItems) {
+        if (listItem.innerText.includes("Yes")) {
+          listItem.click();
+          return;
         }
-      });
-    
-    await typeformFrame.waitForSelector(NEXT_QUESTION_SELECTOR, {timeout: 3000});
-    text = await typeformFrame.$eval(NEXT_QUESTION_SELECTOR, span => {
-      return span.innerText
+      }
+    });
+
+    await typeformFrame.waitForSelector(NEXT_QUESTION_SELECTOR, {
+      timeout: 3000,
+    });
+    text = await typeformFrame.$eval(NEXT_QUESTION_SELECTOR, (span) => {
+      return span.innerText;
     });
     expect(text).to.contain("Question 2");
   });
