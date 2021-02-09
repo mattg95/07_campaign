ae3f792ab967c862bb451ecf044a2ff31ac5fd85
mattg95 <mattgoodman95@gmail.com>
2021-02-09 16:14:52 +0000

    passing spec for mp lookup

--- api-functions.js
+++ api-functions.js
@@ -1,26 +1,37 @@
 require("dotenv").config();
 
-const key = process.env.REACT_APP_TWFY_KEY;
 const axios = require("axios");
-const theyWorkForYouUrl = "https://www.theyworkforyou.com/api/";
+const apiBase =
+  "https://members-api.parliament.uk/api/Members/Search?Location=";
 
 exports.getMpByPostcode = (postcode) => {
-  postcode.replace(/\s/g, "+");
   return axios
-    .get(
-      theyWorkForYouUrl +
-        "getMp?key=" +
-        key +
-        "&postcode=" +
-        postcode +
-        "&output=js"
-    )
+    .get(apiBase + postcode + "&House=Commons")
     .then(({ data }) => {
-      if (data.error) {
-        throw new Error("Error");
-      } else {
-        return data;
-      }
+      const contactInfoArr = data.items[0].links;
+      const contactInfoUrl = contactInfoArr.find(
+        ({ rel }) => (rel = "contactInformation")
+      );
+      return {
+        full_name: data.items[0].value.nameDisplayAs,
+        constituency: data.items[0].value.latestHouseMembership.membershipFrom,
+        party: data.items[0].value.latestParty.name,
+        mpContact: axios
+          .get(
+            `https://members-api.parliament.uk/api${contactInfoUrl.href}/Contact`
+          )
+          .then(({ data }) => {
+            const key = process.env.REACT_APP_TWFY_KEY;
+            const allContactDetailsArr = data.value;
+            const consituencyDetails = allContactDetailsArr.find(
+              ({ type }) => (type = "Constituency")
+            );
+            return (mpObj.email = consituencyDetails.email);
+          })
+          .catch(() => {
+            return { error: "Could not retrieve MP contact details" };
+          }),
+      };
     })
     .catch(() => {
       return { error: "Could not retrieve MP" };
