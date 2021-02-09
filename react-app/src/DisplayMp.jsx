85b243802df7d7409ad77f30d8fbc20e4227afdd
mattg95 <mattgoodman95@gmail.com>
2021-02-09 16:48:36 +0000

    send mp Email address to front end
    -change data strucutre in FE to mirror new api result
    -add tests for mp email addresses
    -fix failing puppeteer tests
    -(auto format puppeteer file)

--- react-app/src/DisplayMp.jsx
+++ react-app/src/DisplayMp.jsx
@@ -1,19 +1,20 @@
 import React from "react";
 
 const DisplayMp = ({
-  mpData: { constituency, full_name, party, name, error },
-  mpEmailAddress,
-}) => (
-  <div className="displayMP" id="displayMP">
-    <h2 className="secondary-header">Find Your MP</h2>
-    <div className="mpCard text-center">
-      <div className="error">{error}</div>
-      <div>{constituency}</div>
-      <div>{name}</div>
-      <div>{full_name}</div>
-      <div>{party}</div>
-      <div className="mpEmailAddress">{mpEmailAddress}</div>
+  mpData: { constituency, full_name, party, name, error, mpEmailAddress },
+}) => {
+  return (
+    <div className="displayMP" id="displayMP">
+      <h2 className="secondary-header">Find Your MP</h2>
+      <div className="mpCard text-center">
+        <div className="error">{error}</div>
+        <div>{constituency}</div>
+        <div>{name}</div>
+        <div>{full_name}</div>
+        <div>{party}</div>
+        <div className="mpEmailAddress">{mpEmailAddress}</div>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 export default DisplayMp;
