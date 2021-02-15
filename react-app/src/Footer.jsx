d1330c5c90368b4c59492fdfd8bd21d84772d8c1
mattg95 <mattgoodman95@gmail.com@>
2021-02-15 20:11:34 +0000

    add privacy policy
    - add tyepform autoclose delay as per sanjay's suggestions
    
    - change title
    
    -styling tweaks

--- react-app/src/Footer.jsx
+++ react-app/src/Footer.jsx
@@ -10,17 +10,20 @@ const Footer = () => (
       </Col>
     </Row>
     <Row>
-      <Col xs={12} lg={6}>
+      <Col xs={12}>
         <Link to="privacy-policy">Privacy Policy</Link>
         {/* <Link to="about"> */}
-        Who we are
+        {/* Who we are */}
         {/* </Link> */}
-      </Col>
-      <Col xs={12} lg={6}>
-        <a href="mailto:email@example.com">email@example.com</a>
-        <a href="https://concepts.effectivealtruism.org/concepts/aid-effectiveness/">
+        © 2021 Red Scarf Services Ltd
+        <br />
+        Company No. 12407870
+        <br />
+        48 Queen Edith's Way, Cambridge, England, CB1 8PW
+        <br />
+        {/* <a href="https://concepts.effectivealtruism.org/concepts/aid-effectiveness/">
           Further reading on aid
-        </a>
+        </a> */}
       </Col>
     </Row>
   </div>
