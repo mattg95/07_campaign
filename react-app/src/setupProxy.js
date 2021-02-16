cf9f38af8070e9d23ef871f7779d8a589b0410fe
mattg95 <mattgoodman95@gmail.com>
2021-02-16 15:22:13 +0000

    try different proxy config

--- react-app/src/setupProxy.js
+++ react-app/src/setupProxy.js
@@ -1,6 +1,4 @@
-const { createProxyMiddleware } = require("http-proxy-middleware");
+const proxy = require("http-proxy-middleware");
 module.exports = function (app) {
-  app.use(
-    createProxyMiddleware("/auth/google", { target: "http://localhost:5000/" })
-  );
+  app.use(proxy(["/auth/google"], { target: "http://localhost:5000/" }));
 };
