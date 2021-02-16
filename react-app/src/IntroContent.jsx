c75d8b2eace93d41afd7487f1e16e68a85f9fe0d
mattg95 <mattgoodman95@gmail.com@>
2021-02-16 12:07:26 +0000

    remove indefinietly

--- react-app/src/IntroContent.jsx
+++ react-app/src/IntroContent.jsx
@@ -11,8 +11,8 @@ const IntroContent = () => (
         "proudly maintain our commitment to spend 0.7 per cent of GNI on
         development"
       </strong>
-      . But just one year later, the government intends to cut foreign aid
-      indefinitely.<br></br>
+      . But just one year later, the government intends to cut foreign aid.
+      <br></br>
       <br></br> At a time of unprecedented international crisis, with millions
       at risk of extreme poverty,{" "}
       <strong>Britain must show leadership - not break its commitments.</strong>
