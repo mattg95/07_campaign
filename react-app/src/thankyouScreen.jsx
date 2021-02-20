89f5c8a9e25b7f760b8b0b0313a3e7ed6e75422d
mattg95 <mattgoodman95@gmail.com>
2021-02-20 12:21:55 +0000

    add thankyou screen
    add emailsent variable
    thankyou appears after email sent

---
+++ react-app/src/thankyouScreen.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+const ThankyouScreen = () => {
+  return (
+    <div>
+      <h2>Thankyou!</h2>
+      <p>Please share this campaign</p>
+      <div
+        class="a2a_kit a2a_kit_size_32 a2a_default_style"
+        data-a2a-url="https://www.point7percent.org/"
+        data-a2a-title="The 0.7% Commitment"
+      >
+        <a class="a2a_dd" href="https://www.addtoany.com/share"></a>
+        <a class="a2a_button_facebook"></a>
+        <a class="a2a_button_twitter"></a>
+        <a class="a2a_button_email"></a>
+        <a class="a2a_button_whatsapp"></a>
+      </div>
+    </div>
+  );
+};
+
+export default ThankyouScreen;
