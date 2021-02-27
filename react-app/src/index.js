07b4e0e86bf63cf82a75868c14a28e629ed03fb4
mattg95 <mattgoodman95@gmail.com@>
2021-02-27 20:13:55 +0000

    import about

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -11,6 +11,7 @@ import reportWebVitals from "./reportWebVitals";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import Footer from "./Footer";
+import About from "./About";
 
 const tagManagerArgs = {
   gtmId: "GTM-MWBT83W",
