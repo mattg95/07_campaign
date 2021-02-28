a38fe32f142a88a789d55dbe70b4d5b52da7b6e1
mattg95 <mattgoodman95@gmail.com>
2021-02-28 10:27:23 +0000

    -add container to footer -resize 'about' page h1

--- react-app/src/Footer.jsx
+++ react-app/src/Footer.jsx
@@ -1,24 +1,26 @@
 import { Link } from "@reach/router";
 import React from "react";
-import { Col, Row } from "react-bootstrap";
+import { Col, Row, Container } from "react-bootstrap";
 
 const Footer = () => (
   <div className="footer" id="Footer">
-    <Row>
-      <Col xs={12}>
-        <Link to="about">About</Link>
-        <Link to="privacy-policy">Privacy Policy</Link>
-        © 2021 The 0.7% Commitment
-        <br />
-        Red Scarf Services Ltd,
-        <br />
-        48 Queen Edith's Way, Cambridge, England, CB1 8PW
-        <br />
-        {/* <a href="https://concepts.effectivealtruism.org/concepts/aid-effectiveness/">
+    <Container>
+      <Row>
+        <Col xs={12}>
+          <Link to="about">About</Link>
+          <Link to="privacy-policy">Privacy Policy</Link>
+          © 2021 The 0.7% Commitment
+          <br />
+          Red Scarf Services Ltd,
+          <br />
+          48 Queen Edith's Way, Cambridge, England, CB1 8PW
+          <br />
+          {/* <a href="https://concepts.effectivealtruism.org/concepts/aid-effectiveness/">
           Further reading on aid
         </a> */}
-      </Col>
-    </Row>
+        </Col>
+      </Row>
+    </Container>
   </div>
 );
 export default Footer;
