f318ebe9b0ec8408780d9e2fce7d20ef0ff9ad6c
mattg95 <mattgoodman95@gmail.com>
2021-06-20 12:01:15 +0100

    move to seperate funcs

--- emailGenerator/index.js
+++ emailGenerator/index.js
@@ -1,7 +1,15 @@
 const { getMpByPostcode } = require("../api-calls");
-const { getRandomResponse } = require("./helper-functions");
+const {
+  getRandomResponse,
+  getAnswerIndex,
+  createGreeting,
+} = require("./helper-functions");
 const { subject, survey, main } = require("./emailStrings.json");
-const { motivationHandler } = require("./responseHandlers");
+const {
+  motivationHandler,
+  countryLinksHandler,
+  religionHandler,
+} = require("./responseHandlers");
 const { religions, questionKeys } = require("./keys");
 
 const generateEmail = ({ answers, definition: { fields } }) => {
@@ -22,26 +30,6 @@ const generateEmail = ({ answers, definition: { fields } }) => {
   ]);
   //these map the question ids from the form onto our json object
 
-  //this gets the index of the answer e.g. in a multiple choice, the first choice is index 0
-  const getAnswerIndex = (idProp) => {
-    let choiceIndex = 0;
-    const thisField = fields.find(({ id }) => id === idProp);
-    const thisAnswers = answers.find(({ field: { id } }) => id === idProp);
-    //this gets the synomys array based on the index of the survey multiple choice options-
-    thisField.choices.forEach((choice, i) => {
-      if (thisAnswers.choice.label === choice.label) {
-        choiceIndex = i;
-      }
-    });
-    return choiceIndex;
-  };
-
-  const createGreeting = ({ name, full_name }) => {
-    salutation = getRandomResponse(main.greeting);
-    const mpName = full_name ? full_name : name;
-    return mpName ? `${salutation} ${mpName},\n\n` : "";
-  };
-
   //this is the 'router' that handles all question responses based on their id
 
   answers.forEach(({ text, field, choice, phone_number }) => {
@@ -53,40 +41,20 @@ const generateEmail = ({ answers, definition: { fields } }) => {
     }
     if (field.id === "EejpFBEzP9wK") {
       //conservatives handler
-      const choiceIndex = getAnswerIndex("EejpFBEzP9wK");
+      const choiceIndex = getAnswerIndex("EejpFBEzP9wK", fields, answers);
       // The first 3 choices for survey.conservative have sentences in emailStrings.json about being a conservative
       memberOfConservatives = choiceIndex < 4;
     }
 
     //religion handler
     if (field.id === "IdqRPd6SUMVh") {
-      const choiceIndex = getAnswerIndex("IdqRPd6SUMVh");
-      if ([7, 8].includes(choiceIndex)) return;
-      else {
-        const { adj, noun } = religions[choiceIndex];
-        let sentence = getRandomResponse(survey.religion);
-        sentence = sentence
-          .replace("RELIGIOUS_DENONYM_NOUN", noun)
-          .replace("RELIGIOUS_DENONYM_ADJ", adj);
-        emailMap.set("religion", sentence);
-      }
+      const religions = religionHandler("IdqRPd6SUMVh", fields, answers);
+      emailMap.set("religion", religions);
     }
     //countryLinksHandler
     if (field.id === "Z4awe4sDljLR") {
-      const choiceIndex = getAnswerIndex("Z4awe4sDljLR");
-      const choiceObj = survey[questionKeys["Z4awe4sDljLR"]][choiceIndex];
-      if (choiceObj === undefined) return;
-      else {
-        const sentence = getRandomResponse(choiceObj.synonyms);
-        const countryNameData = answers.find(
-          ({ field: { id } }) => id === "MRPxTl6j1QAw"
-        );
-        const sentenceWithCountry = sentence.replace(
-          /COUNTRY_NAME/g,
-          countryNameData.text
-        );
-        emailMap.set("countryLinks", sentenceWithCountry);
-      }
+      const counryLinks = countryLinksHandler("Z4awe4sDljLR", fields, answers);
+      emailMap.set("countryLinks", counryLinks);
     }
     // moivations handler
     if (field.id === "wKGNjgRDml1H") {
@@ -95,13 +63,13 @@ const generateEmail = ({ answers, definition: { fields } }) => {
     }
     //meetMp handler
     if (field.id === "vdZgYVyiLE13") {
-      if (getAnswerIndex("vdZgYVyiLE13") === 0) {
+      if (getAnswerIndex("vdZgYVyiLE13", fields, answers) === 0) {
         emailMap.set("meetMp", getRandomResponse(survey.meetMp));
       }
     }
     //meetMp double check hanlder
     if (field.id === "UhNb2Z5nqHtb") {
-      if (getAnswerIndex("UhNb2Z5nqHtb") === 0) {
+      if (getAnswerIndex("UhNb2Z5nqHtb", fields, answers) === 0) {
         emailMap.set("meetMp", getRandomResponse(survey.meetMp));
       }
     }
@@ -133,7 +101,7 @@ const generateEmail = ({ answers, definition: { fields } }) => {
 
   return getMpByPostcode(postcode.text).then((mp) => {
     if (memberOfConservatives && mp.party === "Conservative") {
-      const choiceIndex = getAnswerIndex("EejpFBEzP9wK");
+      const choiceIndex = getAnswerIndex("EejpFBEzP9wK", fields, answers);
       const choiceObj = survey[questionKeys["EejpFBEzP9wK"]][choiceIndex];
       if (choiceObj.synonyms.length > 0) {
         emailMap.set("conservative", getRandomResponse(choiceObj.synonyms));
--- emailGenerator/responseHandlers.js
+++ emailGenerator/responseHandlers.js
@@ -1,6 +1,6 @@
-const { getRandomResponse } = require("./helper-functions");
+const { getRandomResponse, getAnswerIndex } = require("./helper-functions");
 const { survey } = require("./emailStrings.json");
-const { questionKeys } = require("./keys");
+const { questionKeys, religions } = require("./keys");
 
 const motivationHandler = (thisId, fields, answers) => {
   const thisField = fields.find(({ id }) => id === thisId);
@@ -21,4 +21,34 @@ const motivationHandler = (thisId, fields, answers) => {
   return sentenceArr.length ? sentenceArr.join(" ") : "";
 };
 
-module.exports = { motivationHandler };
+const countryLinksHandler = (thisId, fields, answers) => {
+  const choiceIndex = getAnswerIndex(thisId, fields, answers);
+  const choiceObj = survey[questionKeys[thisId]][choiceIndex];
+  if (choiceObj === undefined) return "";
+  else {
+    const sentence = getRandomResponse(choiceObj.synonyms);
+    const countryNameData = answers.find(
+      ({ field: { id } }) => id === "MRPxTl6j1QAw"
+    );
+    const sentenceWithCountry = sentence.replace(
+      /COUNTRY_NAME/g,
+      countryNameData.text
+    );
+    return sentenceWithCountry;
+  }
+};
+
+const religionHandler = (thisId, fields, answers) => {
+  const choiceIndex = getAnswerIndex(thisId, fields, answers);
+  if ([7, 8].includes(choiceIndex)) return "";
+  else {
+    const { adj, noun } = religions[choiceIndex];
+    let sentence = getRandomResponse(survey.religion);
+    sentence = sentence
+      .replace("RELIGIOUS_DENONYM_NOUN", noun)
+      .replace("RELIGIOUS_DENONYM_ADJ", adj);
+    return sentence;
+  }
+};
+
+module.exports = { motivationHandler, countryLinksHandler, religionHandler };
